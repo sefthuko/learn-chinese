a82e8daa4b279801cd906a1880c7ed3cf21f720e
Hermann Chong <hchong@gmail.com>
2018-02-18 21:21:38 -0800

    Python's pretty neat!  This way I don't have to type as much.

--- bin/generate.py
+++ bin/generate.py
@@ -1,9 +1,33 @@
 #!/bin/python
 
 import os
+import yaml
+from hanziconv import HanziConv
+import pinyin
 
 def parse_index():
-    return {}
+    with open("index.yaml") as idx:
+        return yaml.load(idx, Loader=yaml.Loader)
+
+def write_index(index, f):
+    if "categories" not in index:
+        return
+
+    for category in index["categories"]:
+        if "cn" in category:
+            cn = category["cn"]
+            zh = HanziConv.toTraditional(cn)
+            py = pinyin.get(cn, delimiter=" ")
+        elif "zh" in category:
+            zh = category["zh"]
+            cn = HanziConv.toSimplified(zh)
+            py = pinyin.get(zh, delimiter=" ")
+        f.write("<ul>\n")
+        f.write("<li>\n")
+        f.write("{}<br>{}<br>{}<br>{}".format(category["en"], cn, zh, py))
+        write_index(category, f) 
+        f.write("</li>\n")
+        f.write("</ul>\n")
 
 def parse_content():
     return {}
@@ -12,8 +36,14 @@ def write_content(model):
     pass
 
 if __name__ == "__main__":
-    os.mkdir("build")
-    index = parse_index()
+    try:
+        os.mkdir("build")
+    except FileExistsError as e:
+        pass
+
+    with open("build/index.html", "w") as o:
+        write_index(parse_index(), o)
+
     model = parse_content()
 
     write_content(model)
