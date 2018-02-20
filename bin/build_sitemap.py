19d1255484ff57e916e9d917869f59646574b323
Hermann Chong <hchong@gmail.com>
2018-02-19 21:05:33 -0800

    Add relative links to the sitemap

--- bin/build_sitemap.py
+++ bin/build_sitemap.py
@@ -9,11 +9,32 @@ def parse_sitemap():
     with open("sitemap.yaml") as sitemap:
         return yaml.load(sitemap, Loader=yaml.Loader)
 
-def write_sitemap(sitemap, f):
-    if "categories" not in sitemap:
-        return
+def parse_story(fn):
+    with open(fn) as story:
+        return yaml.load(story, Loader=yaml.Loader)
 
-    for category in sitemap["categories"]:
+def write_sitemap(sitemap, f):
+    if "categories" in sitemap:
+        write_categories(sitemap["categories"], f)
+    elif "stories" in sitemap:
+        write_stories(sitemap["stories"], f)
+
+def write_stories(stories, f):
+    f.write("<ul>\n")
+    for story in stories:
+        segments = "/".join(story["id"].split("."))
+        yaml_path = "content/{}.yaml".format(segments)
+        s = parse_story(yaml_path)
+
+        f.write("""<li>
+    <a href="{}_trad.html">{} (traditional)</a><br>
+    <a href="{}_simp.html">{} (simplified)</a></li>
+""".format(segments, s["title"]["en"], segments, s["title"]["en"]))
+
+    f.write("</ul>\n")
+
+def write_categories(categories, f):
+    for category in categories:
         if "cn" in category:
             cn = category["cn"]
             zh = HanziConv.toTraditional(cn)
