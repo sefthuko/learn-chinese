8f3316e375b9e234757cacd8c4772dff7f9984ad
Hermann Chong <hchong@gmail.com>
2018-03-11 21:33:07 -0700

    Moving content to the right part of the repo

--- bin/build_index.py
+++ bin/build_index.py
@@ -4,8 +4,8 @@ from genshi.template import TemplateLoader
 import os
 
 if __name__ == "__main__":
-    loader = TemplateLoader(os.path.abspath("templates"), auto_reload=True)
+    loader = TemplateLoader([os.path.abspath("templates"), os.path.abspath("content")])
 
-    tmpl = loader.load("index.html")
+    tmpl = loader.load("index.xml")
     with open("build/index.html", "w") as f:
         f.write(tmpl.generate().render('html', doctype='html'))
